ff4b60236c37c9ad9432d6ae0c733705a722133b
Tsvetkov Vladimir <tsvetkov.vldmr@gmail.com>
2024-01-08 23:19:19 +0300

    add tg handler

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const token = "6703355257:AAGbmXkZwHQ4Tn-z8tGnMAAd_1VhI9mpzpE"
 
 func main() {
-	fmt.Println("hello ")
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	// Optional: wait for updates and clear them if you don't want to handle
+	// a large backlog of old messages
+	time.Sleep(time.Millisecond * 500)
+	updates.Clear()
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		_, _ = bot.Send(msg)
+	}
 }
